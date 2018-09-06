51f5b4274014f8cfabd953863c623e6a401ec509
CodeDotJS <rushforlinux@gmail.com>
2018-09-06 09:41:25 +0530

    feature fix #1

--- cli.js
+++ cli.js
@@ -49,30 +49,32 @@ dns.lookup('instagram.com', err => {
 		logUpdate(`\n${pos} Please check your Internet Connection! \n`);
 	} else {
 		logUpdate();
-		spinner.text = `Hold your breath, sucker!`;
+		spinner.text = 'Hold your breath, sucker!';
 		spinner.start();
 
 		got(profile).then(res => {
 			let message = '';
-
-			const usr = res.body.split(`"external_url":`)[1].split(',"')[0] === 'null' ? message += chalk.red('no external url found') : message += res.body.split(`"external_url":"`)[1].split(`","`)[0];
+			const privacy = res.body.split('"is_private":')[1].split(',"')[0] === 'false' ? 'Public' : 'Private';
+			const usr = res.body.split('"external_url":')[1].split(',"')[0] === 'null' ? message += chalk.red('no external url found') : message += res.body.split('"external_url":"')[1].split('","')[0];
 
 			logUpdate(`
-${pre} Full Name      :  ${strip(res.body.split('full_name":"')[1].split(`","`)[0]) || chalk.red(`${arg}'s full name is not available!`)}
+${pre} Full Name      :  ${strip(res.body.split('full_name":"')[1].split('","')[0]) || chalk.red(`${arg}'s full name is not available!`)}
 
-${pre} Posts          :  ${res.body.split(`,"edge_owner_to_timeline_media":{"count":`)[1].split(`,"`)[0]}
+${pre} Profile        :  ${privacy}
 
-${pre} Biography      :  ${strip(res.body.split(`"biography":"`)[1].split(`","`)[0]) || `${chalk.red('no biography found')}`}
+${pre} Posts          :  ${res.body.split(',"edge_owner_to_timeline_media":{"count":')[1].split(',"')[0]}
 
-${pre} Followers      :  ${res.body.split(`,"edge_followed_by":{"count":`)[1].split(`},"`)[0]}
+${pre} Biography      :  ${strip(res.body.split('"biography":"')[1].split('","')[0]) || `${chalk.red('no biography found')}`}
 
-${pre} Following      :  ${res.body.split(`,"edge_follow":{"count":`)[1].split(`},"`)[0]}
+${pre} Followers      :  ${res.body.split(',"edge_followed_by":{"count":')[1].split('},"')[0]}
+
+${pre} Following      :  ${res.body.split(',"edge_follow":{"count":')[1].split('},"')[0]}
 
 ${pre} External link  :  ${usr}
 				`);
 			spinner.stop();
-		}).catch(err => {
-			if (err) {
+		}).catch(error => {
+			if (error) {
 				logUpdate(`\n${pos} ${arg} is not an Instagram user! \n`);
 				process.exit(1);
 			}
